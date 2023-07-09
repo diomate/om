f55898cba896abade6f9530d3c3b7a9de6f1c811
diomate <131899154+diomate@users.noreply.github.com>
2023-07-09 15:38:26 +0530

    change

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const LoginPage = () => {
           <input type="password" value={password} onChange={handlePasswordChange} required />
         </div>
         <div>
-          <button type="submit">Sign Up</button>
+          <button type="submit">click Here</button>
         </div>
       </form>
     </div>
